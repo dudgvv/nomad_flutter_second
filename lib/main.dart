52b5d93ef6dcee392419b2fdbf22d872185ec27d
dudgvv <dudg0820@gmail.com>
2024-01-01 04:17:44 +0900

    firebase 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nomad_flutter_second/firebase_options.dart';
 import 'package:nomad_flutter_second/router.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
